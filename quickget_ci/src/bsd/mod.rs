9e033b2f0b58d7b46106dac2a7e5dc13356f74d8
Liam <33645555+lj3954@users.noreply.github.com>
2024-05-27 22:30:40 -0700

    Add functional FreeBSD config generation

---
+++ quickget_ci/src/bsd/mod.rs
@@ -0,0 +1,65 @@
+use crate::store_data::{ArchiveFormat, Config, Distro, Source, WebSource};
+use crate::utils::capture_page;
+use quickemu::config::{Arch, GuestOS};
+use regex::Regex;
+
+const FREEBSD_X86_64_RELEASES: &str = "https://download.freebsd.org/ftp/releases/amd64/amd64/";
+const FREEBSD_AARCH64_RELEASES: &str = "https://download.freebsd.org/ftp/releases/arm64/aarch64/";
+const FREEBSD_EDITIONS: [&str; 2] = ["disc1", "dvd1"];
+
+pub struct FreeBSD {}
+impl Distro for FreeBSD {
+    const NAME: &'static str = "freebsd";
+    const PRETTY_NAME: &'static str = "FreeBSD";
+    const HOMEPAGE: Option<&'static str> = Some("https://www.freebsd.org/");
+    const DESCRIPTION: Option<&'static str> = Some("Operating system used to power modern servers, desktops, and embedded platforms.");
+    fn generate_configs(&self) -> Vec<Config> {
+        // TODO: Add riscv64
+        let mut releases: Vec<Config> = Vec::new();
+        let freebsd_regex = Regex::new(r#"href="([0-9\.]+)-RELEASE"#).unwrap();
+        if let Some(page) = capture_page(FREEBSD_X86_64_RELEASES) {
+            for capture in freebsd_regex.captures_iter(&page) {
+                let release = &capture[1];
+                let checksum_url = format!("{FREEBSD_X86_64_RELEASES}ISO-IMAGES/{release}/CHECKSUM.SHA512-FreeBSD-{release}-RELEASE-amd64");
+                let checksums = capture_page(&checksum_url);
+                for edition in FREEBSD_EDITIONS {
+                    let iso = format!("FreeBSD-{release}-RELEASE-amd64-{edition}.iso.xz");
+                    let checksum = checksums.as_ref().and_then(|c| {
+                        c.lines()
+                            .find(|l| l.contains(&iso))
+                            .and_then(|l| l.split_once(" = ").map(|(_, c)| c.to_string()))
+                    });
+                    let iso_url = format!("{FREEBSD_X86_64_RELEASES}ISO-IMAGES/{release}/{iso}");
+                    releases.push(Config {
+                        guest_os: GuestOS::FreeBSD,
+                        iso: Some(vec![Source::Web(WebSource::new(iso_url, checksum, Some(ArchiveFormat::Xz)))]),
+                        ..Default::default()
+                    });
+                }
+            }
+        };
+        if let Some(page) = capture_page(FREEBSD_AARCH64_RELEASES) {
+            for capture in freebsd_regex.captures_iter(&page) {
+                let release = &capture[1];
+                let checksum_url = format!("{FREEBSD_AARCH64_RELEASES}ISO-IMAGES/{release}/CHECKSUM.SHA512-FreeBSD-{release}-RELEASE-arm64-aarch64");
+                let checksums = capture_page(&checksum_url);
+                for edition in FREEBSD_EDITIONS {
+                    let iso = format!("FreeBSD-{release}-RELEASE-arm64-aarch64-{edition}.iso.xz");
+                    let checksum = checksums.as_ref().and_then(|c| {
+                        c.lines()
+                            .find(|l| l.contains(&iso))
+                            .and_then(|l| l.split_once(" = ").map(|(_, c)| c.to_string()))
+                    });
+                    let iso_url = format!("{FREEBSD_AARCH64_RELEASES}ISO-IMAGES/{release}/{iso}");
+                    releases.push(Config {
+                        guest_os: GuestOS::FreeBSD,
+                        arch: Arch::aarch64,
+                        iso: Some(vec![Source::Web(WebSource::new(iso_url, checksum, Some(ArchiveFormat::Xz)))]),
+                        ..Default::default()
+                    });
+                }
+            }
+        };
+        releases
+    }
+}
--- quickget_ci/src/main.rs
+++ quickget_ci/src/main.rs
@@ -1,5 +1,11 @@
+mod bsd;
 mod store_data;
+mod utils;
 
 fn main() {
-    println!("Hello, world!");
+    let freebsd = bsd::FreeBSD {};
+    let os: store_data::OS = freebsd.into();
+    println!("{}", serde_json::to_string(&os).unwrap());
+    println!("\n\n\nPRETTY:\n\n");
+    println!("{}", serde_json::to_string_pretty(&os).unwrap());
 }
