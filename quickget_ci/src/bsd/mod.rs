4b127b3a5435fe5d56ddcf51b17da7c8fff02ee8
Liam <33645555+lj3954@users.noreply.github.com>
2024-05-28 12:11:08 -0700

    Asynchronously fetch pages

--- quickget_ci/src/bsd/mod.rs
+++ quickget_ci/src/bsd/mod.rs
@@ -2,6 +2,7 @@ use crate::store_data::{ArchiveFormat, Config, Distro, Source, WebSource};
 use crate::utils::capture_page;
 use quickemu::config::{Arch, GuestOS};
 use regex::Regex;
+use tokio::spawn;
 
 const FREEBSD_X86_64_RELEASES: &str = "https://download.freebsd.org/ftp/releases/amd64/amd64/";
 const FREEBSD_AARCH64_RELEASES: &str = "https://download.freebsd.org/ftp/releases/arm64/aarch64/";
@@ -13,9 +14,9 @@ impl Distro for FreeBSD {
     const PRETTY_NAME: &'static str = "FreeBSD";
     const HOMEPAGE: Option<&'static str> = Some("https://www.freebsd.org/");
     const DESCRIPTION: Option<&'static str> = Some("Operating system used to power modern servers, desktops, and embedded platforms.");
-    fn generate_configs(&self) -> Vec<Config> {
+    async fn generate_configs(&self) -> Vec<Config> {
+        let mut futures = Vec::new();
         // TODO: Add riscv64
-        let mut releases: Vec<Config> = Vec::new();
         let freebsd_regex = Regex::new(r#"href="([0-9\.]+)-RELEASE"#).unwrap();
         let find_checksum = |checksums: Option<&str>, iso: &str| {
             checksums.and_then(|c| {
@@ -24,45 +25,60 @@ impl Distro for FreeBSD {
                     .and_then(|l| l.split_once(" = ").map(|(_, c)| c.to_string()))
             })
         };
-        if let Some(page) = capture_page(FREEBSD_X86_64_RELEASES) {
+        if let Some(page) = capture_page(FREEBSD_X86_64_RELEASES).await {
             for capture in freebsd_regex.captures_iter(&page) {
-                let release = &capture[1];
-                let checksum_url = format!("{FREEBSD_X86_64_RELEASES}ISO-IMAGES/{release}/CHECKSUM.SHA512-FreeBSD-{release}-RELEASE-amd64");
-                let checksums = capture_page(&checksum_url);
-                for edition in FREEBSD_EDITIONS {
-                    let iso = format!("FreeBSD-{release}-RELEASE-amd64-{edition}.iso.xz");
-                    let checksum = find_checksum(checksums.as_deref(), &iso);
-                    let url = format!("{FREEBSD_X86_64_RELEASES}ISO-IMAGES/{release}/{iso}");
-                    releases.push(Config {
-                        guest_os: GuestOS::FreeBSD,
-                        iso: Some(vec![Source::Web(WebSource::new(url, checksum, Some(ArchiveFormat::Xz), None))]),
-                        ..Default::default()
-                    });
-                }
+                let release = capture[1].to_owned();
+                futures.push(spawn(async move {
+                    let checksum_url = format!("{FREEBSD_X86_64_RELEASES}ISO-IMAGES/{release}/CHECKSUM.SHA512-FreeBSD-{release}-RELEASE-amd64");
+                    let checksums = capture_page(&checksum_url).await;
+                    FREEBSD_EDITIONS
+                        .iter()
+                        .map(|edition| {
+                            let iso = format!("FreeBSD-{release}-RELEASE-amd64-{edition}.iso.xz");
+                            let checksum = find_checksum(checksums.as_deref(), &iso);
+                            let url = format!("{FREEBSD_X86_64_RELEASES}ISO-IMAGES/{release}/{iso}");
+                            Config {
+                                guest_os: GuestOS::FreeBSD,
+                                iso: Some(vec![Source::Web(WebSource::new(url, checksum, Some(ArchiveFormat::Xz), None))]),
+                                release: Some(release.to_string()),
+                                edition: Some(edition.to_string()),
+                                ..Default::default()
+                            }
+                        })
+                        .collect::<Vec<Config>>()
+                }));
             }
         } else {
             log::warn!("Failed to fetch FreeBSD x86_64 releases");
         }
-        if let Some(page) = capture_page(FREEBSD_AARCH64_RELEASES) {
+        if let Some(page) = capture_page(FREEBSD_AARCH64_RELEASES).await {
             for capture in freebsd_regex.captures_iter(&page) {
-                let release = &capture[1];
-                let checksum_url = format!("{FREEBSD_AARCH64_RELEASES}ISO-IMAGES/{release}/CHECKSUM.SHA512-FreeBSD-{release}-RELEASE-arm64-aarch64");
-                let checksums = capture_page(&checksum_url);
-                for edition in FREEBSD_EDITIONS {
-                    let iso = format!("FreeBSD-{release}-RELEASE-arm64-aarch64-{edition}.iso.xz");
-                    let checksum = find_checksum(checksums.as_deref(), &iso);
-                    let url = format!("{FREEBSD_AARCH64_RELEASES}ISO-IMAGES/{release}/{iso}");
-                    releases.push(Config {
-                        guest_os: GuestOS::FreeBSD,
-                        arch: Arch::aarch64,
-                        iso: Some(vec![Source::Web(WebSource::new(url, checksum, Some(ArchiveFormat::Xz), None))]),
-                        ..Default::default()
-                    });
-                }
+                let release = capture[1].to_owned();
+                futures.push(spawn(async move {
+                    let checksum_url = format!("{FREEBSD_AARCH64_RELEASES}ISO-IMAGES/{release}/CHECKSUM.SHA512-FreeBSD-{release}-RELEASE-arm64-aarch64");
+                    let checksums = capture_page(&checksum_url).await;
+                    FREEBSD_EDITIONS
+                        .iter()
+                        .map(|edition| {
+                            let iso = format!("FreeBSD-{release}-RELEASE-arm64-aarch64-{edition}.iso.xz");
+                            let checksum = find_checksum(checksums.as_deref(), &iso);
+                            let url = format!("{FREEBSD_AARCH64_RELEASES}ISO-IMAGES/{release}/{iso}");
+                            Config {
+                                guest_os: GuestOS::FreeBSD,
+                                arch: Arch::aarch64,
+                                iso: Some(vec![Source::Web(WebSource::new(url, checksum, Some(ArchiveFormat::Xz), None))]),
+                                release: Some(release.to_string()),
+                                edition: Some(edition.to_string()),
+                                ..Default::default()
+                            }
+                        })
+                        .collect::<Vec<Config>>()
+                }));
             }
         } else {
             log::warn!("Failed to fetch FreeBSD aarch64 releases");
         }
-        releases
+        let releases = futures::future::join_all(futures).await;
+        releases.into_iter().flatten().flatten().collect::<Vec<Config>>()
     }
 }
--- quickget_ci/src/main.rs
+++ quickget_ci/src/main.rs
@@ -1,11 +1,15 @@
 mod bsd;
+mod linux;
 mod store_data;
 mod utils;
 
-fn main() {
+use store_data::ToOS;
+
+#[tokio::main]
+async fn main() {
     env_logger::Builder::new().filter_level(log::LevelFilter::Debug).init();
     let freebsd = bsd::FreeBSD {};
-    let os: store_data::OS = freebsd.into();
+    let os: store_data::OS = freebsd.to_os().await;
     println!("{}", serde_json::to_string(&os).unwrap());
     println!("\n\n\nPRETTY:\n\n");
     println!("{}", serde_json::to_string_pretty(&os).unwrap());
