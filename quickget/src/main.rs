72edb5a4f314d5184f33a0bc3aeac4d56e9d1167
Liam <33645555+lj3954@users.noreply.github.com>
2024-06-01 18:09:25 -0700

    Changes to quickget-rs structure

--- quickget/src/main.rs
+++ quickget/src/main.rs
@@ -2,8 +2,10 @@ mod parse_data;
 
 use parse_data::get_json_contents;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let json_data = get_json_contents()?;
+
 
     todo!()
 }
