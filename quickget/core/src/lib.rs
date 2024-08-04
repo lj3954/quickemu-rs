eed013a4d5069f96037b25c40ab96317edc36ea5
Liam <33645555+lj3954@users.noreply.github.com>
2024-08-03 20:27:49 -0700

    feat: Expose error types

--- quickget/core/src/lib.rs
+++ quickget/core/src/lib.rs
@@ -4,4 +4,5 @@ mod config_search;
 mod error;
 mod instance;
 pub use config_search::{ConfigSearch, QuickgetConfig};
+pub use error::{ConfigSearchError, DLError};
 pub use instance::{QGDockerSource, QGDownload, QuickgetInstance};
