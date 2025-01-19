7d75f4232dc59201ec50f3d7ddf99023f5207ec4
Liam <33645555+lj3954@users.noreply.github.com>
2025-01-19 15:42:54 -0800

    test: Add basic CLI to test core functionality

---
+++ quickemu/cli/src/main.rs
@@ -0,0 +1,9 @@
+use std::path::Path;
+
+use quickemu_core::config::Config;
+fn main() {
+    let config_file = std::env::args().nth(1).expect("config file");
+    let config = Config::parse(Path::new(&config_file)).expect("config file");
+    println!("{:#?}", config);
+    println!("{:#?}", config.to_qemu_args());
+}
